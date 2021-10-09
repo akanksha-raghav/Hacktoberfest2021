48fd260a1c998a86201155653af720f77bd39167
isha <56998032+Isha307@users.noreply.github.com>
2021-10-09 09:27:23 +0530

    Delete Solutio.py

--- Programs/Isha Shaw/Solutio.py
+++
@@ -1,9 +0,0 @@
-class Solution(object):
-    def longestOnes(self, A, k):
-        i = 0
-        for j in xrange(len(A)):
-            k -= A[j] == 0
-            if k < 0:
-                k += A[i] == 0
-                i += 1
-        return j - i + 1
