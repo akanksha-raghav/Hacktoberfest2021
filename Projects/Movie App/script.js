08c04ecf4bbf8b01e6961948460ea0fbcf3dd349
Pratibha Singh <63441963+Crazy2code15@users.noreply.github.com>
2021-10-15 12:51:12 +0530

    Update script.js

--- Projects/Movie App/script.js
+++ Projects/Movie App/script.js
@@ -1,6 +1,3 @@
-/******************************************
- *  Author : Pratibha Singh  
- *******************************************/
 
 const GET_API = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=3fd2be6f0c70a2a598f084ddfb75487c&page=2';
 const IMG_PATH = 'https://image.tmdb.org/t/p/w1280';
