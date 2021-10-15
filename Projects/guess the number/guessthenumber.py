d3ba96a665fe3c1fdc34749f356b4ad6f03e13f1
deepanshu sharma <you@example.com>
2021-10-15 17:16:00 +0530

    Added guess the number game - python program

---
+++ Projects/guess the number/guessthenumber.py
@@ -0,0 +1,13 @@
+import random
+
+max_num = int(input("Enter the maximum no- "))
+
+random_number = random.randint(1, max_num)
+guess = 0
+while guess != random_number:
+    guess = int(input(f"Guess the number between 1 & {max_num}: "))
+    if guess < random_number:
+        print("Wrong! Too low...")
+    elif guess > random_number:
+        print("Wrong! Too high...")
+print(f"Thats Right! Random number is {random_number}")
