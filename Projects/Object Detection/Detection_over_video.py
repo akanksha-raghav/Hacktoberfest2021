d192448b3a9b43966047feaf2c7f004ec3a76b1e
souvik0306 <souvikdatta123@gmail.com>
2021-10-21 17:44:22 +0530

    TensorFlow Titanic Dataset

--- Projects/Object Detection/Detection_over_video.py
+++ Projects/Object Detection/Detection_over_video.py
@@ -1,15 +1,15 @@
 import cv2 as cv
 import numpy as np
 
-config_file = (r'C:\Users\souvi\Hacktoberfest2021\Projects\Object Detection\Coco\ssd_mobilenet_v3_large_coco_2020_01_14.pbtxt')
-frozen_model = (r'C:\Users\souvi\Hacktoberfest2021\Projects\Object Detection\Coco\frozen_inference_graph.pb')
+config_file = ('Coco\ssd_mobilenet_v3_large_coco_2020_01_14.pbtxt')
+frozen_model = ('Coco\frozen_inference_graph.pb')
 out = cv.VideoWriter('Result.mp4', cv.VideoWriter_fourcc(*'m', 'p', '4', 'v'), 20, (700, 500))
 
 
 model = cv.dnn_DetectionModel(frozen_model, config_file)
 
 classLabels = []
-file_name = (r'C:\Users\souvi\Hacktoberfest2021\Projects\Object Detection\Labels.txt')
+file_name = ('Labels.txt')
 with open(file_name, 'rt') as fpt:
     classLabels = fpt.read().rstrip('\n').split('\n')
 
